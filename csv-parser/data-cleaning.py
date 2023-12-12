c16b45bbf4c5bf691e680e5a35d1c23d0a236f88
nrghosh <nrghosh@wesleyan.edu>
2023-12-11 23:36:50 -0500

    standardize classification types and clean up

--- csv-parser/data-cleaning.py
+++ csv-parser/data-cleaning.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from fuzzywuzzy import process
 
 # Set the path to the CSV file
 csv_file_path = "annotated-dataset/human-annotated-data-raw.csv"
@@ -9,7 +10,18 @@ data = pd.read_csv(csv_file_path)
 # Standardize values in the 'classification' column to lowercase
 data['classification'] = data['classification'].str.lower()
 
-# Extract unique values of the 'classification' column after standardization
+# Define the standard labels
+standard_labels = ['affirm', 'grant', 'deny', 'reverse', 'remand', 'defer', 'other']
+
+# Function for fuzzy matching and selecting the best match
+def match_label(value):
+    match, score = process.extractOne(value, standard_labels)
+    return match if score >= 80 else value
+
+# Apply fuzzy matching to the 'classification' column
+data['classification'] = data['classification'].apply(match_label)
+
+# Extract unique values of the 'classification' column after mapping
 unique_classification = data['classification'].unique()
 
 # Print the unique standardized values
