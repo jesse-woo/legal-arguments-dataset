2bb56b5b3e512170b02c0c33352272493291cf78
nrghosh <nrghosh@wesleyan.edu>
2023-12-12 00:12:47 -0500

    basic plots for salience and classification distribution

--- csv-parser/analysis.py
+++ csv-parser/analysis.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Set the path to the standardized CSV file
 standardized_file_path = "annotated-dataset/human-annotated-data-standardized.csv"
@@ -24,3 +26,43 @@ print(salience_stats)
 
 print("\nClassification and Salience Relationship:")
 print(classification_salience_relationship)
+
+# Get the total count for each classification type
+total_classification_count = len(data)
+
+# Visualize the distribution of each classification type and save as an image
+fig_classification = plt.figure(figsize=(10, 6))
+ax_classification = sns.countplot(x='classification', data=data, order=data['classification'].value_counts().index)
+plt.title('Distribution of Classification Types')
+plt.xlabel('Classification Types')
+plt.ylabel('Count')
+
+# Annotate count values on top of each bar
+for p in ax_classification.patches:
+    ax_classification.annotate(f'{p.get_height()}', (p.get_x() + p.get_width() / 2., p.get_height()),
+                                ha='center', va='center', xytext=(0, 10), textcoords='offset points')
+
+# Add a text annotation for the total count
+plt.text(0.5, -0.1, f'Total N = {total_classification_count}', ha='center', va='center', transform=ax_classification.transAxes)
+
+fig_classification.savefig("classification_distribution.png")
+
+# Get the total count for each salience rating
+total_salience_count = len(data)
+
+# Visualize the distribution of each salience rating and save as an image
+fig_salience = plt.figure(figsize=(10, 6))
+ax_salience = sns.countplot(x='salience-rating', data=data, order=data['salience-rating'].value_counts().index)
+plt.title('Distribution of Salience Ratings')
+plt.xlabel('Salience Ratings')
+plt.ylabel('Count')
+
+# Annotate count values on top of each bar
+for p in ax_salience.patches:
+    ax_salience.annotate(f'{p.get_height()}', (p.get_x() + p.get_width() / 2., p.get_height()),
+                         ha='center', va='center', xytext=(0, 10), textcoords='offset points')
+
+# Add a text annotation for the total count
+plt.text(0.5, -0.1, f'Total N = {total_salience_count}', ha='center', va='center', transform=ax_salience.transAxes)
+
+fig_salience.savefig("salience_distribution.png")
